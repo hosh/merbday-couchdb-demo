7b5d7c4d183cd971aa80aa246dcf7d1374d8b93f
Ho-Sheng Hsiao <hosh@isshen.com>
2008-11-08 18:41:11 -0500

    [PROFILES] Added contact info and skills to profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,11 @@
+class ContactInfo < CouchRest::Model
+  key_accessor :phone, :email, :twitter
+end
+
+class Skill < CouchRest::Model
+  key_accessor :skill, :expertise, :skilled_since
+end
+
 class Profile < CouchRest::Model
   use_database CouchRest.database!('http://localhost:5984/profiles-test')
 
@@ -6,7 +14,11 @@ class Profile < CouchRest::Model
 
   key_accessor :name, :user_id
 
+  # belongs_to :user
   view_by :user_id, :updated_at
   view_by :user_id
+
+  cast :contact_info, :as => 'ContactInfo'
+  cast :skills, :as => ['Skill']
 
 end
