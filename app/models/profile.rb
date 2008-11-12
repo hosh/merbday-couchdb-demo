4b477675a903b192f3e5d5541e00c38b77466e46
Ho-Sheng Hsiao <hosh@isshen.com>
2008-11-12 15:56:49 -0500

    [PROFILES] Made Skill/ContactInfo under the "Profile" namespace

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,19 +1,20 @@
-class ContactInfo < CouchRest::Model
-  key_accessor :phone, :email, :twitter
-end
-
-class Skill < CouchRest::Model
-  key_accessor :skill, :expertise
-
-  key_writer :skilled_since 
-  def skilled_since
-    Time.parse(self['skilled_since'])
-  end
-
-end
-
 class Profile < CouchRest::Model
   use_database CouchRest.database!('http://localhost:5984/profiles-test')
+
+  class ContactInfo < CouchRest::Model
+    key_accessor :phone, :email, :twitter
+  end
+
+  class Skill < CouchRest::Model
+    key_accessor :skill, :expertise
+
+    key_writer :skilled_since 
+    def skilled_since
+      Time.parse(self['skilled_since'])
+    end
+
+  end
+
 
   key_reader :slug, :created_at, :updated_at
   timestamps!
@@ -24,8 +25,8 @@ class Profile < CouchRest::Model
   view_by :user_id, :updated_at
   view_by :user_id
 
-  cast :contact_info, :as => 'ContactInfo'
-  cast :skills, :as => ['Skill']
+  cast :contact_info, :as => 'Profile::ContactInfo'
+  cast :skills, :as => ['Profile::Skill']
 
 
   def all_contacts
